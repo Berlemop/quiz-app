f0cb91ff2811d9c0c10a959b5ea5f2854c395a2f
berlemop <paul.berlemont@edu.esiee.fr>
2025-10-03 18:34:45 +0200

    new

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import jwt
+import datetime
+
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +15,13 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+
+    return jsonify({"token": token}), 200
+
 
 if __name__ == "__main__":
     app.run()
