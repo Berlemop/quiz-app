0eaec55e675c7e4a1d47ff04f6ba8edcda13ff0d
berlemop <paul.berlemont@edu.esiee.fr>
2025-10-07 18:39:37 +0200

    mise en place de l'authentification

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import jwt
 import datetime
+import jwt_utils
 
 
 app = Flask(__name__)
 CORS(app)
+
+ADMIN_PASSWORD = "mdp" 
 
 @app.route('/')
 def hello_world():
@@ -18,10 +21,19 @@ def GetQuizInfo():
 
 @app.route('/login', methods=['POST'])
 def login():
-    
 
-    return jsonify({"token": token}), 200
+	payload = request.get_json() 
 
+	if not payload or 'password' not in payload:
+		return 'Unauthorized', 401
+	    
+	password = payload['password']
+
+	if password == ADMIN_PASSWORD:
+		token = jwt_utils.build_token()
+		return jsonify({"token": token}), 200
+	else:
+		return 'Unauthorized', 401 
 
 if __name__ == "__main__":
     app.run()
